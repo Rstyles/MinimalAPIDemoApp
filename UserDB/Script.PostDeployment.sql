c5857b524a88d8c2eb98220396b5ff072d4a0018
Ryan Styles <ryan.c.styles@gmail.com>
2022-01-03 20:23:42 -0600

    Add project files.

---
+++ UserDB/Script.PostDeployment.sql
@@ -0,0 +1,9 @@
+﻿if not exists (select 1 from dbo.[User])
+begin
+	insert into dbo.[user] (FirstName, LastName)
+	values ('Ryan', 'Styles'),
+	('Dave', 'Victorine'),
+	('Emmanuel', 'Osei Kwame'),
+	('Dusty', 'Hope'),
+	('Toby', 'Watkins');
+end
